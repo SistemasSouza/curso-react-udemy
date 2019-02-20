fed676feff5e8859a25d175a5511158405a11153
Denis Souza <denyssouza1@hotmail.com>
2019-02-20 00:44:06 -0300

    initial commit

---
+++ exercicios_react/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+module.exports = {
+    mode: 'development',
+    entry:'./ex/index.js',
+    output:{
+        path:path.resolve(__dirname,'public'),
+        filename: './bundle.js'
+    },
+    devServer:{
+        port:8080,
+        contentBase:path.join(__dirname,'public'),
+        compress:true
+    },
+    module:{
+        rules: [
+            {
+              test: /\.m?js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env',"@babel/preset-react"],
+                  plugins: ['@babel/plugin-proposal-object-rest-spread']
+                }
+              }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
+    }
+}
